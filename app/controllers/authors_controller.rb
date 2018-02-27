9aca6f528fb69bd0a38e9ebf87309bdd8d4b3290
porzingod <jasonlim.ny@gmail.com>
2018-02-27 12:54:18 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,13 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    begin
+      @author = Author.create!(author_params)
+    rescue
+      render :new
+    else
+      redirect_to author_path(@author)
+    end
   end
 
   private
