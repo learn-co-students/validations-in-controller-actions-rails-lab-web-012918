45048510801627d65fc4965d98df864385f14421
harenarium <22226614+harenarium@users.noreply.github.com>
2018-02-27 14:00:47 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,17 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
 
-    redirect_to post_path(@post)
+
+    @post.assign_attributes(post_params)
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render "edit"
+    end
+    
+
   end
 
   private
